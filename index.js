87f5be5a497b7991591148def41f27e63251d4b2
arpit <blaganarpit@gmail.com>
2024-01-03 15:25:17 +0530

    done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express=require('express');
+require("dotenv").config();
 const app=express();
 const cors=require('cors');
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://Arpit:Ab123@cluster0.j4fl22k.mongodb.net/assi').then(()=>{
+mongoose.connect(process.env.URL).then(()=>{
     console.log("connected");
 })
 const pDB=require('./schema');
@@ -10,17 +11,27 @@ app.use(express.json());
 app.use(cors());
 app.post("/add",async(req,res)=>{
     const {pickedUp,assigned_to_batch,products}=req.body;
-    console.log(products);   
-    const data=await pDB.find({'products':{$in:products}});
+    //console.log(products);   
+    const pro= Object.values(products.reduce((acc, obj) => {
+        acc[obj.name] = obj;
+        return acc;
+      }, {}));
+    console.log("uniq",pro);
+    const productNames = pro.map(product => product.name); 
+    const data=await pDB.find({'products.name':{$in:productNames}});
+    console.log("data",data);
     const val=[];
-    const uni=new Set();
-    products.map((ele)=>{
-        if(!uni.has(ele.name)&&!data.includes(ele)){
-            console.log(ele);uni.add(ele.name);
-            val.push(ele);
-        }
-    });
-    const dd=await pDB.create({pickedUp,assigned_to_batch,products:val});
+    pro.forEach((ele)=>{
+        let f=false;
+        data.forEach((el)=>{
+            el.products.forEach((e)=>{
+                if(e.name==ele.name){f=true;}
+            })
+        });
+        if(!f){val.push(ele);}
+    });console.log(val);
+    if(val.length){
+    const dd=await pDB.create({pickedUp,assigned_to_batch,products:val});}
     const ddd=await pDB.find();
     res.json(ddd);
 })
